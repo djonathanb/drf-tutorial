0ec3bef86c5f09e2954e3d9d77c23a41ecc3f642
Djonathan Barros <djonathan.barros@gmail.com>
2019-05-17 18:57:57 -0300

    implemented hiperlinked relations

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -7,23 +7,24 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
-class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer for System User.
-    """
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'snippets')
-
-
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     """
     Serializer for Snippet model.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer for System User.
+    """
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'id', 'username', 'snippets')
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,10 +5,25 @@ Rest views for snippets module.
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    """
+    API entry point
+    """
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
 
 
 class UserList(generics.ListAPIView):
@@ -25,6 +40,18 @@ class UserDetail(generics.RetrieveAPIView):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    """
+    Retrieve a snippet highlight
+    """
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
